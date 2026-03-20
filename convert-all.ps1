2724a84055cb8ce9f7becae3408352838d35a6f1
Takeshi Yano <tyano.line@gmail.com>
2026-03-20 14:49:24 +0900

    Rename

---
+++ convert-all.ps1
@@ -0,0 +1,12 @@
+Get-ChildItem $args[0] -Recurse -Filter *.ja-Jp.*.ttml | ForEach-Object {
+    $filePath = $_.FullName
+    # $filePath の .ja-jp～.ttmlを.mp4に置換
+    $newFilePath = $filePath -replace '\.ja-Jp\..*\.ttml$', '.ass'
+    # $newFilePath が存在するかどうかをチェック
+    if (Test-Path $newFilePath) {
+        Write-Output "File $newFilePath already exists. Skipping $filePath"
+    } else {
+        Write-Output "Converting $filePath to $newFilePath"
+        python "$PSScriptRoot\convert_ttml_to_ass.py" $filePath $newFilePath
+    }
+}
